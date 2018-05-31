597679bb554b3da11510707a3f0d831a36c38672
s-r-aman <s.r.aman.sra@gmail.com>
2018-05-31 23:36:57 +0530

    Added default text.

--- src/components/UI/Text.js
+++ src/components/UI/Text.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-const Head1 = ({ children, styles: extraStyles, ...rest }) => (
+const Head1 = ({ children, style: extraStyles, ...rest }) => (
   <Text {...rest} style={[styles.head1, extraStyles]}>
+    {children}
+  </Text>
+);
+
+const TextMod = ({ children, style: extraStyles, ...rest }) => (
+  <Text {...rest} style={[styles.text, extraStyles]}>
     {children}
   </Text>
 );
@@ -11,7 +17,11 @@ const styles = StyleSheet.create({
   head1: {
     fontSize: 25,
     fontWeight: 'bold'
+  },
+  text: {
+    color: '#bbb',
+    backgroundColor: 'transparent'
   }
 });
 
-export { Head1 };
+export { Head1, TextMod };
