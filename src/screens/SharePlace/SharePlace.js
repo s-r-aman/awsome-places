36cf2dfa5852bf8ebd6a340b3bb1c37fd283182a
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-02 23:44:24 +0530

    Changed laout  (image and map preview added.)

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, View, Button } from 'react-native';
-
-import PlaceInput from './../../components/PlaceInput/PlaceInput';
+import {
+  Text,
+  View,
+  Button,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+  Image
+} from 'react-native';
+import { DefaultInput } from './../../components/UI/Inputs';
+import { Head1, TextMod } from './../../components/UI/Text';
+import image from './../../assets/beautiful-place.jpg';
 import { addPlace } from './../../store/actions/index.js';
 
 class SharePlaceScreen extends Component {
@@ -26,13 +35,56 @@ class SharePlaceScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Share Place Screen</Text>
-        <PlaceInput onPlaceAdded={this.placeAddHandler} />
-      </View>
+      <ScrollView>
+        <View style={style.container}>
+          <Head1 style={style.heading}>
+            <TextMod>Share A Place With Us!</TextMod>
+          </Head1>
+          <View style={style.placeHolder}>
+            <Image source={image} style={style.image} />
+          </View>
+          <View style={style.button}>
+            <Button title="Pick Image" />
+          </View>
+          <View style={style.placeHolder}>
+            <Text>Map Preview!</Text>
+          </View>
+          <View style={style.button}>
+            <Button title="Locate Me" />
+          </View>
+          <DefaultInput placeholder="Place name." />
+          <View style={style.button}>
+            <Button title="Share the place." />
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const style = StyleSheet.create({
+  placeHolder: {
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#eee',
+    width: '80%',
+    height: 150
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  button: {
+    margin: 8
+  },
+  heading: {
+    margin: 15
+  },
+  image: {
+    height: '100%',
+    width: '100%'
+  }
+});
 
 const mapDispatchToProps = dispatch => ({
   onAddPlace: placeName => dispatch(addPlace(placeName))
