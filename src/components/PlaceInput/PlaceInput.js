bc3d5b913e49d73485090f3ca13b293b1c8e9442
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-04 22:09:41 +0530

    Refactored comp.

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -2,26 +2,11 @@ import React, { Component } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 import { DefaultInput } from './../UI/Inputs';
-class PlaceInput extends Component {
-  state = {
-    placeName: ''
-  };
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  render() {
-    return (
-      <DefaultInput
-        placeholder="Place name."
-        value={this.state.placeName}
-        onChangeText={this.placeNameChangedHandler}
-      />
-    );
-  }
-}
-
-export default PlaceInput;
+export default ({ placeName, placeNameChangedHandler}) => (
+  <DefaultInput
+    placeholder="Place name."
+    value={placeName}
+    onChangeText={placeNameChangedHandler}
+  />
+);
